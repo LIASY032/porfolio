957a4fcd957a2f35fa318eb6b08b8211ad6bd647
LIASY032 <liasy032@mymail.unisa.edu.au>
2021-06-25 14:14:09 +0930

    change the background color

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,16 @@
 import React from "react";
+import "./common.css";
 
 function About() {
   return (
-    <>
+    <div className="common-container">
       <h1>About me</h1>
       <p>Hi Guys,</p>
       <p>
         I am Shiwei Liang coming from China. Now I am studying in University of
         South Australia.
       </p>
-    </>
+    </div>
   );
 }
 
