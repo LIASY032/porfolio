ee033157d2c25776fcd51ea778d64167fef94c1c
LIASY032 <liasy032@mymail.unisa.edu.au>
2021-06-26 22:50:34 +0930

    delete Container, Row, Col

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./common.css";
-import { Container, Row, Col } from "react-bootstrap";
 
 function About() {
   return (
@@ -16,22 +15,6 @@ function About() {
         </div>
         <div>this is animation part.</div>
       </div>
-
-      {/* <Container>
-        <Row className="justify-content-md-center">
-          <Col>
-            <h1>About me</h1>
-            <p>Hi Guys,</p>
-            <p>
-              I am Shiwei Liang coming from China. Now I am studying in
-              University of South Australia.
-            </p>
-          </Col>
-          <Col>
-            <div>this is animation part.</div>
-          </Col>
-        </Row>
-      </Container> */}
     </div>
   );
 }
