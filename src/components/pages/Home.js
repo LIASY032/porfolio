985da57cba5592c8628d1e4eb3f45c797e13c094
LIASY032 <liasy032@mymail.unisa.edu.au>
2021-06-29 15:40:16 +0930

    change the home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Circle, Square, Triangle } from "react-bootstrap-icons";
 import "./home.css";
+import "./common.css";
 
 function Home() {
   return (
     <>
-      <div className="spinner">
+      <div className="common-container">
         <Triangle className="triangle position-absolute pattern-size " />
 
         <Square className="square position-absolute pattern-size " />
@@ -13,7 +14,27 @@ function Home() {
         <Circle className="circle position-absolute pattern-size " />
         <div className="content">
           <div>
-            <label>Shiwei Liang</label>
+            <div className="text-container">
+              <span>H</span>
+              <span>i, </span>
+            </div>
+            <div className="text-container">
+              <span>I </span>
+              <span>a</span>
+              <span>m </span>
+
+              <span>S</span>
+              <span>h</span>
+              <span>i</span>
+              <span>w</span>
+              <span>e</span>
+              <span>i </span>
+              <span>L</span>
+              <span>i</span>
+              <span>a</span>
+              <span>n</span>
+              <span>g</span>
+            </div>
           </div>
 
           <div>
