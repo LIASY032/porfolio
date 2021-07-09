a504813cf10ff9c6de46849a68a93dc99be6b895
LIASY032 <liasy032@mymail.unisa.edu.au>
2021-07-09 23:18:50 +0930

    fixing the bugs

---
+++ src/components/ContactInput.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Form, FormLabel, Col, Row, FormFloating } from "react-bootstrap";
+
+function ContactInput() {
+  return (
+    <Form>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+    </Form>
+  );
+}
+
+export default ContactInput;
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,32 +1,37 @@
 import React from "react";
 import "./common.css";
 import { Fade } from "react-reveal";
+import { Container, Row, Col } from "react-bootstrap";
+import ContactInput from "../ContactInput";
 function Contact() {
   return (
-    <div className="flex-container">
-      <Fade bottom cascade={true}>
-        <div className="text-container">
-          <span>C</span>
-          <span>o</span>
-          <span>n</span>
-          <span>t</span>
-          <span>a</span>
-          <span>c</span>
-          <span>t </span>
+    <Container className="common-container padding-top-half">
+      <Row>
+        <Col>
+          <Fade bottom cascade={true}>
+            <div className="text-container">
+              <span>C</span>
+              <span>o</span>
+              <span>n</span>
+              <span>t</span>
+              <span>a</span>
+              <span>c</span>
+              <span>t </span>
 
-          <span>m</span>
-          <span>e</span>
-        </div>
-      </Fade>
-
-      <div>
-        <p>I am interested in frontend or backend opportunities</p>
-        <p>
-          If you want to contact me for jobs, please write the "job" in
-          "Subject"
-        </p>
-      </div>
-    </div>
+              <span>m</span>
+              <span>e</span>
+            </div>
+          </Fade>
+          <p>I am interested in frontend or backend opportunities</p>
+          <p>
+            If you want to contact me for jobs, please write the "job" in
+            "Subject"
+          </p>
+          <ContactInput></ContactInput>
+        </Col>
+        <Col>Show the map</Col>
+      </Row>
+    </Container>
   );
 }
 
