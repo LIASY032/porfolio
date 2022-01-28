8b0f527858291f8df4204119e7be7cc834cf4532
LIASY032 <66517361+LIASY032@users.noreply.github.com>
2022-01-28 17:36:08 +1030

    Gallery added

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,190 @@
+import React from "react";
+import "./style.css";
+function Gallery() {
+  React.useEffect(() => {
+    const IMAGE_URL_PREFIX = "https://assets.codepen.io/1478440/";
+    const MIME_TYPE = ".jpg";
+    const IMAGE_DISPLAY_TIME = 2500;
+    const gallery = window.document.getElementById("gallery");
+    const gridItems = Array.prototype.slice.call(
+      window.document.getElementsByClassName("flip-inner")
+    );
+    const activeImages = [];
+    const vBirds = [
+      "goldeneagle01",
+      "hummingbird01",
+      "kestrel01",
+      "lilacroller01",
+      "macaw01",
+      "osprey01",
+      "parrot01",
+      "peacock01",
+      "woodduck01",
+      "woodpecker01",
+    ];
+    const hBirds = [
+      "beeeater01",
+      "bluejay01",
+      "bluejay02",
+      "cardinal01",
+      "cockatoo01",
+      "eagle01",
+      "eagle02",
+      "finch01",
+      "flamingo01",
+      "gannet01",
+      "hummingbird01",
+      "kingfisher01",
+      "macaw01",
+      "mandarinduck01",
+      "nuthatch01",
+      "owl01",
+      "owl02",
+      "parrot01",
+      "puffin01",
+      "puffin02",
+      "robin01",
+      "tit01",
+      "toucan01",
+      "warbler01",
+    ];
+    let flipImagesFunc;
+
+    const initImages = () => {
+      const verticalElements = gridItems.filter((gi) =>
+        gi.parentElement.classList.contains("vertical")
+      );
+      const horizontalElements = gridItems.filter((gi) =>
+        gi.parentElement.classList.contains("horizontal")
+      );
+
+      const vBirdsCopy = [...vBirds];
+      for (let el of verticalElements) {
+        for (let img of el.getElementsByTagName("img")) {
+          const bird = getRandomArrayItem(vBirdsCopy);
+          const imageUrl = generateUrl(bird, true);
+          const index = vBirdsCopy.indexOf(bird);
+          img.src = imageUrl;
+          vBirdsCopy.splice(index, 1);
+          activeImages.push(bird);
+        }
+      }
+
+      const hBirdsCopy = [...hBirds];
+      for (let el of horizontalElements) {
+        for (let img of el.getElementsByTagName("img")) {
+          const bird = getRandomArrayItem(hBirdsCopy);
+          const imageUrl = generateUrl(bird, false);
+          const index = hBirdsCopy.indexOf(bird);
+          img.src = imageUrl;
+          hBirdsCopy.splice(index, 1);
+          activeImages.push(bird);
+        }
+      }
+
+      gallery.style.display = "flex";
+    };
+
+    const flipImage = () => {
+      let gridItem = getRandomArrayItem(gridItems);
+
+      var imgs;
+      if (gridItem.dataset.rotated === "true") {
+        gridItem.style.transform = "none";
+        gridItem.dataset.rotated = "false";
+        imgs = gridItem.getElementsByClassName("back");
+      } else {
+        gridItem.style.transform = "rotateY(180deg)";
+        gridItem.dataset.rotated = "true";
+        imgs = gridItem.getElementsByClassName("front");
+      }
+
+      const isVertical = gridItem.parentNode.classList.contains("vertical");
+      const newBird = getNewBird(isVertical);
+      const newUrl = generateUrl(newBird, isVertical);
+      const oldUrl = imgs[0].src;
+      const oldBird = oldUrl.substring(
+        IMAGE_URL_PREFIX.length + 2,
+        oldUrl.length - MIME_TYPE.length
+      );
+
+      // once flipped change the backface image to a new one, in prep for the next flip
+      imgs[0].src = newUrl;
+      activeImages[activeImages.indexOf(oldBird)] = newBird;
+    };
+
+    const getNewBird = (isVertical) => {
+      let images = isVertical ? vBirds : hBirds;
+      return getRandomArrayItem(
+        images.filter((i) => !activeImages.includes(i))
+      );
+    };
+
+    const generateUrl = (bird, isVertical) => {
+      return isVertical
+        ? IMAGE_URL_PREFIX.concat("v_", bird, MIME_TYPE)
+        : IMAGE_URL_PREFIX.concat("h_", bird, MIME_TYPE);
+    };
+
+    const getRandomArrayItem = (array) => {
+      return array[Math.floor(Math.random() * array.length)];
+    };
+
+    window.document.addEventListener("visibilitychange", function () {
+      if (window.document.hidden) {
+        window.clearInterval(flipImagesFunc);
+      } else {
+        flipImagesFunc = window.setInterval(flipImage, IMAGE_DISPLAY_TIME);
+      }
+    });
+
+    initImages();
+    flipImagesFunc = setInterval(flipImage, IMAGE_DISPLAY_TIME);
+  }, []);
+  return (
+    <>
+      <section id="gallery" className="gallery">
+        <div className="side horizontal flip-outer">
+          <div id="1" className="flip-inner">
+            <img className="front" alt="" />
+            <img className="back" alt="" />
+          </div>
+        </div>
+        <div className="center">
+          <div className="vertical top flip-outer">
+            <div id="2" className="flip-inner">
+              <img className="front" alt="" />
+              <img className="back" alt="" />
+            </div>
+          </div>
+          <div className="horizontal top flip-outer">
+            <div id="3" className="flip-inner">
+              <img className="front" alt="" />
+              <img className="back" alt="" />
+            </div>
+          </div>
+          <div className="horizontal bottom flip-outer">
+            <div id="4" className="flip-inner">
+              <img className="front" alt="" />
+              <img className="back" alt="" />
+            </div>
+          </div>
+          <div className="vertical bottom flip-outer">
+            <div id="5" className="flip-inner">
+              <img className="front" alt="" />
+              <img className="back" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="side horizontal flip-outer">
+          <div id="6" className="flip-inner">
+            <img className="front" alt="" />
+            <img className="back" alt="" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Gallery;
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,25 +1,17 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 
-import Item from "../components/Item";
 import Title from "../components/Title";
-
+import Gallery from "../components/Gallery";
 function Project() {
   return (
     <div className="common-container">
-      <Container className="text-center">
-        <Row>
-          <Title>Projects</Title>
-        </Row>
-        <Row>
-          <Col>
-            <Item />
-          </Col>
-          <Col>
-            <Item />
-          </Col>
-        </Row>
-      </Container>
+      <Row>
+        <Title>Projects</Title>
+      </Row>
+      <Row>
+        <Gallery />
+      </Row>
     </div>
   );
 }
