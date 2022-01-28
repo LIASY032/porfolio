13189b6bc12bfa729357eb050024e0791cb5e309
LIASY032 <66517361+LIASY032@users.noreply.github.com>
2022-01-28 19:35:03 +1030

    Update Project.js

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -7,25 +7,23 @@ import Items from "../components/Items";
 function Project() {
   return (
     <div className="common-container">
-      <Row>
+      <div style={{ textAlign: "center" }}>
         <Title>Projects</Title>
-      </Row>
-      <Row>
-        <Items>
-          <Items.Item
-            header="React Shopping Cart"
-            img="https://user-images.githubusercontent.com/66517361/129296452-ea1f187c-d052-4fe5-8ab5-3a6712ece44d.png"
-          >
-            It provides the basic functions and user friendly interface for
-            customers.
-          </Items.Item>
-          <Items.Item header="6-Eleven" img="images/6_eleven.png">
-            Built with modern web frameworks, the project consists of key
-            components of a commercial E-commerce website that aims to provide a
-            rich user experience.
-          </Items.Item>
-        </Items>
-      </Row>
+      </div>
+      <Items>
+        <Items.Item
+          header="React Shopping Cart"
+          img="https://user-images.githubusercontent.com/66517361/129296452-ea1f187c-d052-4fe5-8ab5-3a6712ece44d.png"
+        >
+          It provides the basic functions and user friendly interface for
+          customers.
+        </Items.Item>
+        <Items.Item header="6-Eleven" img="images/6_eleven.png">
+          Built with modern web frameworks, the project consists of key
+          components of a commercial E-commerce website that aims to provide a
+          rich user experience.
+        </Items.Item>
+      </Items>
     </div>
   );
 }
