8c8fda434e518b96a25a9ce8628a9b37d3b85b82
LIASY032 <66517361+LIASY032@users.noreply.github.com>
2022-02-15 21:46:09 +1030

    check Canvas

--- src/components/Background/Embers/MyCanvas.js
+++ src/components/Background/Embers/MyCanvas.js
@@ -1,32 +1,26 @@
-import React from 'react';
-import './style.css';
-
+import React from "react";
+import "./style.css";
 
 function MyCanvas() {
-
-
-
-
   return (
     <>
       <div id="myCanvasContainer">
         <canvas width="300" height="300" id="myCanvas">
-        
+          <div id="tags" style={{ fontSize: "50%" }}>
+            <a href="/about">npm</a>
+            <a href="/about">NodeJS</a>
+
+            <a href="/about">Javascript</a>
+            <a href="/about">Python</a>
+            <a href="/about">Java</a>
+            <a href="/about">C#</a>
+            <a href="/about">C++</a>
+            <a href="/about">TypeScript</a>
+            <a href="/about">html</a>
+            <a href="/about">css</a>
+            <a href="/about">scss</a>
+          </div>
         </canvas>
-      </div>
-      <div id="tags" style={{ fontSize: '50%' }}>
-      <a href="/about">npm</a>
-      <a href="/about">NodeJS</a>
-      
-      <a href="/about">Javascript</a>
-      <a href="/about">Python</a>
-      <a href="/about">Java</a>
-      <a href="/about">C#</a>
-      <a href="/about">C++</a>
-      <a href="/about">TypeScript</a>
-      <a href="/about">html</a>
-      <a href="/about">css</a>
-      <a href="/about">scss</a>
       </div>
     </>
   );
